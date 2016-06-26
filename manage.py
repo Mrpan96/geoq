45848ee4fe8776edc98013f642b2df661f45ae76
Qiyin Pan <panqiyin@sina.com>
2016-06-26 20:02:19 +0800

    Update manage.py
    test branch

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geoq.settings")
-
+    print("I'm comming")
     manage_dir = os.path.dirname(os.path.realpath(__file__))
     sys.path.append(os.path.join(manage_dir, 'geoq'))
 
